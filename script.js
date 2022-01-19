827e3086abf03acdd729ff4e29e81ffaede1fe9a
Chioma Onukogu <conukogu1@gmail.com>
2022-01-18 21:57:36 -0500

    added the html and js pages for this week's challenges

--- script.js
+++ script.js
@@ -9,20 +9,31 @@ console.log(`The difference between ${x} and ${y} is ${x-y}`);
 a=window.prompt("Type a name.");
 b=window.prompt("Type another name.");
 
-if (Math.max(a.length,b.length)===a.length && a.length-b.length===1)
-{console.log(`The name ${a} is longer than ${b} by ${a.length-b.length} character`)}
+if (Math.max(a.length,b.length)===a.length && a.length-b.length===1){
+    console.log(`The name ${a} is longer than ${b} by ${a.length-b.length} character`)
+    document.getElementById("name").innerHTML=`The name ${a} is longer than ${b} by ${a.length-b.length} character`
+}
 
-else if (Math.max(a.length,b.length)===a.length && a.length-b.length>1)
-{console.log(`The name ${a} is longer than ${b} by ${a.length-b.length} characters`)}
 
-else if (Math.max(a.length,b.length)===b.legth && b.length-a.length===1)
-{console.log(`The name ${b} is longer than ${a} by ${b.length-a.length} character`)}
+else if (Math.max(a.length,b.length)===a.length && a.length-b.length>1){
+    console.log(`The name ${a} is longer than ${b} by ${a.length-b.length} characters`)
+    document.getElementById("name").innerHTML=`The name ${a} is longer than ${b} by ${a.length-b.length} characters`
+}
 
-else if (Math.max(a.length,b.length)===b.length && b.length-a.length>1)
-{console.log(`The name ${b} is longer than ${a} by ${b.length-a.length} characters`)}
+else if (Math.max(a.length,b.length)===b.legth && b.length-a.length===1){
+    console.log(`The name ${b} is longer than ${a} by ${b.length-a.length} character`)
+    document.getElementById("name").innerHTML=`The name ${b} is longer than ${a} by ${b.length-a.length} character`
+}
 
-else 
-{console.log(`The name ${a} and ${b} are the same length of characters`)};
+else if (Math.max(a.length,b.length)===b.length && b.length-a.length>1){
+    console.log(`The name ${b} is longer than ${a} by ${b.length-a.length} characters`)
+    document.getElementById("name").innerHTML=`The name ${b} is longer than ${a} by ${b.length-a.length} characters`
+}
+
+else {
+    console.log(`The name ${a} and ${b} are the same length of characters`)
+    document.getElementById("name").innerHTML=`The name ${a} and ${b} are the same length of characters`
+};
 
 
 c=window.prompt("Enter a word");
@@ -100,7 +111,104 @@ while (z===0)
     else
     {
     h=window.prompt("ERROR! Please enter a valid operator! \n(Your options are: \naddition[+] \nsubtraction[-] \nmultiplication[*] \ndivision[/]")
+    }}
+
+    //Time to review Arrays!
+
+    //Arrays can be innitialized in two ways:
+    //The first is var NAME=[];
+    //The second is var NAME=new Array();
+    //Arrays can hold all a lot of types of info.
+    var numbers = [19, 17, 3, 7, 21, 23, 6];
+
+    var names = ["Patrick", "Zendaya", "Daniel", "Chioma", "Iman"];
+
+    var games = new Array ("MW2", "Overwatch", "2K15", "Sims3");
+
+    console.log(numbers[4]); //This will print 21 bc the #21 is the 4th numeral in the array. Remeber that arrays start counting at 0;
+
+    var myPod = ["Brenden", "Puran", "Chioma", "Yasmine", "Giselle"];
+
+    console.log(`There are ${myPod.length} people inside of ${myPod[2]}'s group.`)
+
+    var roster=window.prompt("Type in a name");
+
+    // if (myPod.includes(roster)===true) <---Superior method imho
+    
+    if (myPod.indexOf(roster)!=-1){
+        console.log(`${roster} is a member of my Pod group`)    
     }
+
+    else {
+        console.log(`${roster} is NOT a member of my Pod group`)    
+    }
+
+
+    /* Reggie's NOTES!
+
+    var numbers = [19, 17, 3, 7, 21, 23, 6]
+
+var names = ["Patrick", "Zendaya", "Daniel", "Chioma", "Iman"]
+
+var games = new Array("MW2", "Overwatch", "2K15", "Sims3", "Crash Bandicoot")
+
+var anime = ["Re:Zero", "Demon Slayer", "Naruto", "Death Note", "Black Clover"]
+
+
+
+var checkNames = names.indexOf("Iman")
+
+if(checkNames !== -1){
+    console.log("They are in this class")
+} else{
+    console.log("They are not in this class")
+}
+
+function checkNames(name){
+
+    var nameCheck = names.indexOf(name)
+
+    if(nameCheck !== -1){
+    console.log(`${name} is in this class`)
+} else{
+    console.log(`${name} is not in this class`)
+}
+}
+
+checkNames("Chioma")
+
+if (names.includes("Patrick")){
+    console.log("Brian is my pod leader");
+} else{
+    console.log("Brian is not my pod leader");
 }
 
 
+for(var i=0; i<numbers.length; i++){
+
+    sum += numbers[i]
+
+}
+console.log(sum)*/
+
+function pathgy (x1, x2)
+{
+function square (x)
+{
+   Math.pow(x, 2) 
+   return x;
+} 
+
+x1=square(x1);
+x2-square(x2);
+
+let x3=x1+x2;
+
+x3=Math.sqrt(x3);
+return x3;
+}
+
+l=5;
+m=5;
+
+console.log(pathgy(l,m));
