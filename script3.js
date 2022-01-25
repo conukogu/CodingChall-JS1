d8455042b062747b888cb249b13f5efe611c0e39
Chioma Onukogu <conukogu1@gmail.com>
2022-01-24 21:47:30 -0500

    add

---
+++ script3.js
@@ -0,0 +1,31 @@
+
+
+var sharePizza = {function (n) {
+ n=cutPizzaSlices()/n;
+  return n;
+}
+}
+
+sharePizza = function cutPizzaSlices(n) {
+  return n;
+}
+
+
+
+sharePizza=cutPizzaSlices(8)
+
+console.log(sharePizza(2));
+
+
+
+
+
+/*var sharePizza = cutPizzaSlices(8);
+
+console.log(sharePizza(2));                                                                                                                                                                    
+
+  // prints "Each person gets 4.00 slices of pizza"
+
+console.log(sharePizza(3)); 
+
+  // prints "Each person gets 2.67 slices of pizza"*/
