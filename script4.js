7a79a42a5800ec258c1d3d5c46cd23c082a47e9a
Chioma Onukogu <conukogu1@gmail.com>
2022-02-02 13:00:03 -0500

    go

--- script4.js
+++ script4.js
@@ -39,3 +39,116 @@ let x = 6;
 let testArr2 = [4,5,6,7,0,1,2];
 
 console.log((pivot(testArr2, x)));
+
+
+
+let a1=67;
+
+let arr1=[7,3,5];
+
+let keep=0;
+let rem=0;
+let record=[];
+let numUsed=[];
+let numAmountIn=[];
+let newTotal=0;
+
+const veryHard=(num, arr) =>
+{
+    arr.sort();
+    for(i=arr.length-1; i>=0; i--)
+    {
+        keep=Math.floor(num/arr[i])
+        record.push(`The number ${arr[i]} goes into ${num} about ${keep} times`)
+        record.push(`Here is the reminader: ${num%arr[i]}`)
+        num=num%arr[i]
+        keepRecord(keep, arr[i]);
+    }
+
+    return record;
+}
+
+
+
+const keepRecord=(keep, arrI) =>
+{
+    //numUsed
+    numUsed.push(arrI)
+    
+    numAmountIn.push(keep)
+}
+
+
+
+    console.log(veryHard(a1, arr1));
+    console.log(`This are the numbers: \n ${numUsed}`);
+
+    console.log(`This are the corresponding amout in: \n ${numAmountIn}`);
+
+    console.log(`This is the new assesment and split: \n ${reasses(numAmountIn)}`);
+
+
+
+
+    function reasses(arr)
+    {
+            let i= arr.length-1;
+
+            while(arr[i]===0 || i===arr.length-1)
+            {i--};
+            arr[i]=arr[i]-1;
+
+            console.log(arr);
+            
+            arr.splice(i+1);
+
+            console.log(arr);
+
+            return arr
+    }
+
+
+    function newNum(valueArr, reassesArr)
+    {
+        let subSum=0;
+
+        for(i=0; i<reassesArr.length; i++)
+            {
+                subSum=valueArr[i]*reassesArr[i]+subSum;
+            }
+
+        return subSum
+    }
+
+
+  newTotal=a1-newNum(numUsed, numAmountIn);
+
+  console.log(`Hey! ${newNum(numUsed, numAmountIn)}`)
+
+  console.log(`Heyo! ${newTotal}`)
+
+
+  function solvingIt (valueArr, reassesArr, newAmount)
+  {
+      for(let i=(valueArr.length-reassesArr.length)-1; i<valueArr.length; i++)
+      {
+          reassesArr.push(Math.floor(newAmount/valueArr[i]))
+          newAmount=newTotal%valueArr[i];
+      }
+      return reassesArr;
+  }
+
+  console.log(solvingIt(numUsed, numAmountIn, newTotal));
+
+  
+  
+  
+  console.log(reasses(numAmountIn));
+
+  newTotal=a1-newNum(numUsed, numAmountIn);
+
+  console.log(`Hey2! ${newNum(numUsed, numAmountIn)}`)
+
+  console.log(`Heyo2! ${newTotal}`)
+
+  console.log(solvingIt(numUsed, numAmountIn, newTotal));
